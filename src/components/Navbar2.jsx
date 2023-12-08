ac5e8ac8494d69c5cae94dd665a0eb5afeed3d04
Veronica Perez <veperez@live.com>
2023-12-08 14:37:31 -0600

    fixed logo route

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import logo from "../assets/images/logo.svg"
+import logo from "../assets/logo.svg"
 
 export default function Navbar2() {
   return (
